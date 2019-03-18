308fe43de33c1825c5f6b2733faf571b9ad7a81f
Pete Keen <pete@petekeen.net>
2019-03-18 09:39:34 -0400

    wtf nginx

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,9 @@
 require 'fileutils'
 
+FileUtils.touch('/tmp/app-initialized')
+
 if ENV['RACK_ENV'] == 'production'
   bind 'unix:///tmp/nginx.socket'
 else
   bind "tcp://0.0.0.0:#{ENV.fetch('PORT', '3000')}"
 end
-
-FileUtils.touch('/tmp/app-initialized')
